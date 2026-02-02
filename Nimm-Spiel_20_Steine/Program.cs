7e37ab411d446384080040f519d84fe2850e86db
Schlagan-fall <alexander.schlagan@gmail.com>
2026-02-02 11:55:12 +0100

    Initial commit ohne .vs Dateien

---
+++ Nimm-Spiel_20_Steine/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nimm_Spiel_20_Steine
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int gesSteine = 20;
+            Console.WriteLine("Es liegen " + gesSteine + " Steine auf dem Tisch!");
+            Console.WriteLine(new String('o', gesSteine));
+
+            Random rand=new Random();
+            int amZug = rand.Next(2);
+
+            Console.WriteLine(amZug == 0 ? "Computer fängt an!" : "Du fängst an!");
+
+            while (gesSteine ==0) 
+            {
+                if (amZug == 0)
+                {
+                    //Gewinnstrategie
+                    int nehmen = (gesSteine - 1) % 4;
+                    
+                    if (nehmen == 0)
+                    {
+                        nehmen = new Random().Next(1, 4);
+                    }
+
+                    Console.WriteLine("Computer nimmt " + nehmen + " Steine.");
+                   
+                    gesSteine -= nehmen;
+
+                    if (gesSteine ==0)
+                    {
+                        Console.WriteLine("********Computer hat verloren!!!********");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Auf dem Tisch bleiben " + gesSteine + " Steine");
+                        Console.WriteLine(new String('o', gesSteine));
+                    }
+
+
+                }
+                else
+                {
+                    int spielerNimmt;
+                    do
+                    {
+                        Console.WriteLine("Wieviele Steine nimmst du (1 bis 3)?");
+                    }    while(!int.TryParse(Console.ReadLine(), out spielerNimmt)
+                            || spielerNimmt < 1 || spielerNimmt >3);
+                    
+                    
+                    gesSteine -= spielerNimmt;
+                    if (gesSteine < 1)
+                    {
+                        Console.WriteLine("********Du hast verloren!!!********");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Auf dem Tisch bleiben " + gesSteine + " Steine");
+                        Console.WriteLine(new String('o', gesSteine));
+                    }
+                    
+                }
+                
+                amZug = 1-amZug;
+               
+
+                 
+            }
+
+
+
+        }
+    }
+}
+
+
---
+++ Nimm-Spiel_20_Steine/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Nimm-Spiel_20_Steine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DAA Deutsche Angestellten-Akademie GmbH")]
+[assembly: AssemblyProduct("Nimm-Spiel_20_Steine")]
+[assembly: AssemblyCopyright("Copyright © DAA Deutsche Angestellten-Akademie GmbH 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("234f4c19-946f-4f40-b54c-4f6447ef13dc")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
